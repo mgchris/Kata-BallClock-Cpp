3971ae4b5e60e1755e6bcbfbc824d2de635bcf0b
Christopher Evans <evanchri@Christophers-MacBook-Pro.local>
2015-09-01 21:26:51 -0600

    Initial Commit

---
+++ BallClock-CPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BallClock-CPP
+//
+//  Created by Christopher Evans on 9/1/15.
+//  Copyright (c) 2015 Morphosis Games. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
